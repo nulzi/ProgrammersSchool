a2fb3d04b42698fd30b77e28d94c907e4127f85c
nulzi <kimty131@naver.com>
2024-04-19 16:26:54 +0900

    load img, css file

--- TENNISMARKET/requestHandler.js
+++ TENNISMARKET/requestHandler.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const main_view = fs.readFileSync("./main.html", "utf-8");
+const main_css = fs.readFileSync("./main.css", "utf-8");
+
 const mariadb = require("./database/connect/mariadb");
 
 function main(response) {
@@ -8,7 +12,13 @@ function main(response) {
   });
 
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.write("Main page");
+  response.write(main_view);
+  response.end();
+}
+
+function mainCss(response) {
+  response.writeHead(200, { "Content-Type": "text/css" });
+  response.write(main_css);
   response.end();
 }
 
@@ -24,9 +34,37 @@ function favicon(response) {
   console.log("favicon");
 }
 
+function racket1(response) {
+  fs.readFile("./img/racket1.png", function (err, data) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
+}
+function racket2(response) {
+  fs.readFile("./img/racket2.png", function (err, data) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
+}
+function racket3(response) {
+  fs.readFile("./img/racket3.png", function (err, data) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
+}
+
 let handle = {};
 handle["/"] = main;
+handle["/main.css"] = mainCss;
 handle["/login"] = login;
 handle["/favicon.ico"] = favicon;
+
+// image directory
+handle["/img/racket1.png"] = racket1;
+handle["/img/racket2.png"] = racket2;
+handle["/img/racket3.png"] = racket3;
 
 exports.handle = handle;
