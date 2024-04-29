1bfcf23e51468ce52a5c002c55592eadbd044e50
nulzi <kimty131@naver.com>
2024-04-29 19:12:26 +0900

    prac - express post

--- BASIC-NODE/app.js
+++ BASIC-NODE/app.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(express.json()); // req.body의 내용 가져오는 모듈
+app.post("/test", (req, res) => {
+  console.log(req.body.message);
+
+  res.json({
+    title: "post test",
+    // message: req.body.message,
+    ...req.body,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
