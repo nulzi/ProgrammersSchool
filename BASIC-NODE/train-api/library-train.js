010fa86b32e4c5a27f8c2b1369b680f16c509584
nulzi <kimty131@naver.com>
2024-04-30 14:09:57 +0900

    2024.04.30 4th week day2

--- BASIC-NODE/train-api/library-train.js
+++ BASIC-NODE/train-api/library-train.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const app = express();
 
@@ -27,9 +28,10 @@ db.set(1, book1);
 db.set(2, book2);
 db.set(3, book3);
 
+// GET 전체 조회
 app.get("/books", (req, res) => {
   // 1. Object.fromEntries()로 Map을 Object로 변환
-  console.log(Object.fromEntries(db));
+  // console.log(Object.fromEntries(db));
   res.json(Object.fromEntries(db));
   // console.log(JSON.stringify(Object.fromEntries(db)));
   // res.json(JSON.stringify(Object.fromEntries(db)));
@@ -45,6 +47,7 @@ app.get("/books", (req, res) => {
   // res.json(JSON.stringify(books));
 });
 
+// GET id로 개별 조회
 app.get("/books/:id", function (req, res) {
   let { id } = req.params;
   id = parseInt(id);
@@ -62,12 +65,71 @@ app.get("/books/:id", function (req, res) {
   }
 });
 
+// POST 책 하나 등록(생성)
 app.post("/book", (req, res) => {
   const id = db.size + 1;
   db.set(id, req.body);
 
   res.json({
-    message: `add new creater ${db.get(id).name}`,
+    message: `add new book ${db.get(id).name}`,
     ...req.body,
   });
+});
+
+// DELETE id로 개별 삭제
+app.delete("/books/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  const removed = db.get(id);
+
+  if (db.delete(id)) {
+    res.json({
+      message: `delete book ${removed.name}`,
+    });
+  } else {
+    res.json({
+      message: `there is no data ${id}`,
+    });
+  }
+});
+
+// DELETE 전체 삭제
+app.delete("/books", (req, res) => {
+  let message = "";
+
+  if (!db.size) {
+    message = "database is already empty";
+  } else {
+    db.clear();
+
+    message = "database is empty";
+  }
+  res.json({
+    message: message,
+  });
+});
+
+// PUT id로 개별 수정
+app.put("/books/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  const before = db.get(id);
+  const { newName } = req.body;
+  if (!before) {
+    res.json({
+      message: `there is no data ${id}`,
+    });
+  } else if (before.name === newName) {
+    res.json({
+      message: `there is no change`,
+    });
+  } else {
+    db.set(id, { ...before, name: newName });
+
+    res.json({
+      message: `change data ${before.name} => ${newName}`,
+    });
+  }
 });
