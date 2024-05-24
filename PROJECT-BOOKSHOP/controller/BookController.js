4585d8bc936c5a9ec0929b54edcb403587559bfa
nulzi <kimty131@naver.com>
2024-05-24 15:54:17 +0900

    기능 추가 - 책 API - 신간 목록 기능 추가 - 페이지 나눠서 출력하는 기능 추가

--- PROJECT-BOOKSHOP/controller/BookController.js
+++ PROJECT-BOOKSHOP/controller/BookController.js
@@ -2,40 +2,45 @@ const mariadb = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const getBookList = (req, res) => {
-  const { categoryId, isNew } = req.query;
-  let sql = `SELECT *, categories.name as category_name FROM books LEFT JOIN categories ON books.category_id = categories.id`;
+  const { categoryId, isNew, limit, pages } = req.query;
+  let sql = `SELECT *, categories.name AS category_name
+  FROM books LEFT JOIN categories
+  ON books.category_id = categories.id`;
+  const offset = limit * (pages - 1);
+  const values = [];
 
-  if (categoryId) {
-    // 카테고리별 책 목록 조회
+  if (categoryId && isNew) {
+    sql += ` WHERE category_id = ? AND published_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW()`;
+    values.push(categoryId);
+  } else if (categoryId) {
     sql += ` WHERE category_id = ?`;
-    return mariadb.query(sql, categoryId, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results);
-      }
-
-      res.status(StatusCodes.NOT_FOUND).end();
-    });
+    values.push(categoryId);
+  } else if (isNew) {
+    sql += ` WHERE published_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW()`;
   }
 
-  // 전체 책 목록 조회
-  mariadb.query(sql, (err, results) => {
+  if (limit && pages) {
+    sql += ` LIMIT ? OFFSET ?`;
+    values.push(parseInt(limit), offset);
+  }
+
+  mariadb.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
 
-    res.status(200).json(results);
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    }
+
+    res.status(StatusCodes.NOT_FOUND).end();
   });
 };
 
 const getBook = (req, res) => {
   const { bookId } = req.params;
-  const sql = `SELECT *, categories.name as category_name FROM books LEFT JOIN categories ON books.category_id = categories.id WHERE books.id = ?`;
+  const sql = `SELECT *, categories.name AS category_name FROM books LEFT JOIN categories ON books.category_id = categories.id WHERE books.id = ?`;
 
   mariadb.query(sql, bookId, (err, results) => {
     if (err) {
@@ -46,7 +51,6 @@ const getBook = (req, res) => {
     const book = results[0];
 
     if (book) {
-      // 개별 책 조회
       return res.status(200).json(book);
     }
 
