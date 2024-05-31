b0edf1837b1a1aae5173b0ae7381f4be7e6b72fc
nulzi <kimty131@naver.com>
2024-05-31 17:57:21 +0900

    기능 추가 - 주문 API - 비동기 추가 - 주문하기 구현(비동기 활용) - 주문한 목록 조회 구현 - 목록 상세 조회 구현

--- PROJECT-BOOKSHOP/controller/OrderController.js
+++ PROJECT-BOOKSHOP/controller/OrderController.js
@@ -1,34 +1,64 @@
 const mariadb = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
-const order = (req, res) => {
+const order = async (req, res) => {
+  const catchError = (error) => {
+    res.status(StatusCodes.BAD_REQUEST).end();
+    throw error;
+  };
   const {
-    basketItems,
+    basketItemIds,
     delivery,
     totalQuantity,
     totalPrice,
     firstBookTitle,
     userId,
   } = req.body;
-  let sql = `INSERT INTO delivery (address, receiver, ph_num) VALUES (?,?,?)`;
+  const mariadbP = require("mysql2/promise");
+  const conn = await mariadbP.createConnection({
+    host: "localhost",
+    port: process.env.DBPORT,
+    user: process.env.USER,
+    password: process.env.PW,
+    database: process.env.DATABASE,
+    dateStrings: true,
+  });
   let values = [delivery.address, delivery.receiver, delivery.ph_num];
-  // let deliveryId;
-  // let orderId;
+  let results = await addDelivery(conn, values).catch(catchError);
+  let deliveryId = results.insertId;
 
-  // mariadb.query(sql, values, (err, results) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(StatusCodes.BAD_REQUEST).end();
-  //   }
+  values = [deliveryId, firstBookTitle, totalPrice, totalQuantity, userId];
+  results = await addOrder(conn, values).catch(catchError);
 
-  //   deliveryId = results.insertId;
-  //   // 결제하기
-  //   res.status(StatusCodes.OK).json(results);
-  // });
+  let orderId = results.insertId;
 
-  // sql = `INSERT INTO orders (delivery_id, book_title, total_price, total_quantity, user_id)
-  // VALUES (?)`;
-  // values = [deliveryId, firstBookTitle, totalPrice, totalQuantity, userId];
+  sql = `SELECT book_id, quantity FROM baskets WHERE id IN (?)`;
+
+  let [basketItems] = await conn.query(sql, [basketItemIds]).catch(catchError);
+
+  values = [];
+  basketItems.forEach((item) => {
+    values.push([orderId, item.book_id, item.quantity]);
+  });
+  results = await addOrderedBook(conn, values).catch(catchError);
+  results = await deleteSelectedBaskets(conn, basketItemIds)
+    .then((result) => {
+      if (!result.affectedRows)
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    })
+    .catch(catchError);
+
+  res.status(StatusCodes.OK).json(results);
+};
+
+const addDelivery = async (conn, values) => {
+  const sql = `INSERT INTO delivery (address, receiver, ph_num) VALUES (?,?,?)`;
+  const [results] = await conn.execute(sql, values);
+  return results;
+};
+
+const addOrder = async (conn, values) => {
+  const sql = `INSERT INTO orders (delivery_id, book_title, total_price, total_quantity, user_id) VALUES (?,?,?,?,?)`;
   // sql = `INSERT INTO orders SET ?`;
   // values = {
   //   delivery_id: 2, //deliveryId
@@ -37,55 +67,63 @@ const order = (req, res) => {
   //   total_quantity: totalQuantity,
   //   user_id: userId,
   // };
-  // mariadb.query(sql, values, (err, results) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(StatusCodes.BAD_REQUEST).end();
-  //   }
-  //   orderId = results.insertId;
-  //   // 결제하기
-  //   res.status(StatusCodes.OK).json(results);
-  // });
+  const [results] = await conn.execute(sql, values);
 
-  sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
-  values = [];
+  return results;
+};
 
-  basketItems.forEach((item) => {
-    values.push([2, item.bookId, item.quantity]);
-  });
+const addOrderedBook = async (conn, values) => {
+  const sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+  const [results] = await conn.query(sql, [values]);
 
-  mariadb.query(sql, [values], (err, results) => {
+  return results;
+};
+
+const deleteSelectedBaskets = async (conn, values) => {
+  const sql = `DELETE FROM baskets WHERE id IN (?)`;
+  const [results] = await conn.query(sql, [values]);
+
+  return results;
+};
+
+const getOrderList = (req, res) => {
+  const { userId } = req.body;
+  const sql = `SELECT orders.id, created_at, address, receiver, ph_num, book_title, total_quantity, total_price
+  FROM orders LEFT JOIN delivery ON orders.delivery_id = delivery.id WHERE user_id = ?`;
+  // const values = [];
+
+  mariadb.query(sql, userId, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
 
-    // 결제하기
-    res.status(StatusCodes.OK).json(results);
-  });
-};
-
-const getOrderList = (req, res) => {
-  const sql = ``;
-  const values = [];
-
-  mariadb.query(sql, (err, results) => {
     // 주문 실제 목록 조회
-    res.status(StatusCodes.OK).json({
-      message: "주문 실제 목록 조회 success",
-    });
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    }
+    res.status(StatusCodes.NOT_FOUND).end();
   });
 };
 
 const getOrderDetail = (req, res) => {
-  const sql = ``;
-  const values = [];
+  const { orderId } = req.params;
+  const { userId } = req.body;
+  const sql = `SELECT book_id, title, img, price, author, quantity
+  FROM orderedBook LEFT JOIN books ON book_id = books.id WHERE order_id = ?`;
+  // const values = [];
 
-  mariadb.query(sql, (err, results) => {
+  mariadb.query(sql, orderId, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
     // 주문 상세 상품 조회
-    res.status(StatusCodes.OK).json({
-      message: "주문 상세 상품 조회 success",
-    });
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    }
+    res.status(StatusCodes.NOT_FOUND).end();
   });
 };
 
