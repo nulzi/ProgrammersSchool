40f6b32cb9af5ba3c8928209dffdf984e8761195
nulzi <kimty131@naver.com>
2024-06-25 18:43:19 +0900

    기능 - BoardList 컴포넌트 추가

---
+++ FE-TASK-MANAGER/src/components/BoardList/BoardList.tsx
@@ -0,0 +1,64 @@
+import React, { FC, useState } from "react";
+import { useTypedSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiPlusCircle } from "react-icons/fi";
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from "./BoardList.css";
+import clsx from "clsx";
+
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const { boardArray } = useTypedSelector((state) => state.boards);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  return (
+    <div className={container}>
+      <div className={title}>게시판:</div>
+      {boardArray.map((board, idx) => (
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[idx].boardId)}
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex((b) => b.boardId == activeBoardId) === idx,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                idx,
+            }
+          )}
+        >
+          <div>{board.boardName}</div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {isFormOpen ? (
+          <SideForm setIsFormOpen={setIsFormOpen} />
+        ) : (
+          <FiPlusCircle className={addButton} onClick={handleClick} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BoardList;
