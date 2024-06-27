f790fbdf1dd5bca7a85e4b10205d526dd33e6518
nulzi <kimty131@naver.com>
2024-06-27 21:16:20 +0900

    기능 - firbase 연동 및 로그인 추가

--- FE-TASK-MANAGER/src/components/BoardList/BoardList.tsx
+++ FE-TASK-MANAGER/src/components/BoardList/BoardList.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from "react";
-import { useTypedSelector } from "../../hooks/redux";
+import { useTypedDispatch, useTypedSelector } from "../../hooks/redux";
 import SideForm from "./SideForm/SideForm";
-import { FiPlusCircle } from "react-icons/fi";
+import { FiLogIn, FiPlusCircle } from "react-icons/fi";
 import {
   addButton,
   addSection,
@@ -11,6 +11,16 @@ import {
   title,
 } from "./BoardList.css";
 import clsx from "clsx";
+import { GoSignOut } from "react-icons/go";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "../../firebase";
+import { removeUser, setUser } from "../../store/slices/userSlice";
+import { useAuth } from "../../hooks/useAuth";
 
 type TBoardListProps = {
   activeBoardId: string;
@@ -23,9 +33,38 @@ const BoardList: FC<TBoardListProps> = ({
 }) => {
   const { boardArray } = useTypedSelector((state) => state.boards);
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const dispatch = useTypedDispatch();
+  const { isAuth } = useAuth();
 
   const handleClick = () => {
     setIsFormOpen(!isFormOpen);
+  };
+  const handleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((userCredential) => {
+        console.log(userCredential);
+        dispatch(
+          setUser({
+            email: userCredential.user.email!,
+            id: userCredential.user.uid,
+          })
+        );
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -55,6 +94,11 @@ const BoardList: FC<TBoardListProps> = ({
           <SideForm setIsFormOpen={setIsFormOpen} />
         ) : (
           <FiPlusCircle className={addButton} onClick={handleClick} />
+        )}
+        {isAuth ? (
+          <GoSignOut className={addButton} onClick={handleLogout} />
+        ) : (
+          <FiLogIn className={addButton} onClick={handleLogin} />
         )}
       </div>
     </div>
---
+++ FE-TASK-MANAGER/src/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwQ8LVscQ3QyPtn5JqPDv8uUEBUPSMAuM",
+  authDomain: "react-task-manager-11164.firebaseapp.com",
+  projectId: "react-task-manager-11164",
+  storageBucket: "react-task-manager-11164.appspot.com",
+  messagingSenderId: "818401916830",
+  appId: "1:818401916830:web:87e3f90d5ebd7fbe33488f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
