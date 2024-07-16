4726898ce43c3757f80e384e7c54407cd7a6c69c
nulzi <kimty131@naver.com>
2024-07-16 13:50:36 +0900

    새로운 프로젝트 book store - 문윤기 강사님

---
+++ FE-BOOKSTORE/bookstore/src/App.tsx
@@ -0,0 +1,18 @@
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { BookStoreThemeProvider } from "./context/themeContext";
+
+function App() {
+  return (
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
+      {/* <Layout children={<Home />} />; */}
+      <Layout>
+        <Home />
+      </Layout>
+    </BookStoreThemeProvider>
+  );
+}
+
+export default App;
---
+++ FE-BOOKSTORE/bookstore/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>book store</div>
+    </>
+  );
+};
+
+export default Home;
