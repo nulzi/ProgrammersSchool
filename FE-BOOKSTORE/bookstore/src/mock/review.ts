5fd843d9506c37df125a241f5b60174f78a6e9b5
nulzi <kimty131@naver.com>
2024-07-25 15:56:02 +0900

    BOOKSTORE - 가짜 리뷰 데이터 만들기 with. fakerjs

--- FE-BOOKSTORE/bookstore/src/mock/review.ts
+++ FE-BOOKSTORE/bookstore/src/mock/review.ts
@@ -1,25 +1,20 @@
 import { BookReviewItem } from "@/models/book.model";
 import { http, HttpResponse } from "msw";
+import { fakerKO as faker } from "@faker-js/faker";
+
+const mockReviewData: BookReviewItem[] = Array.from({ length: 8 }).map(
+  (_, i) => ({
+    id: i,
+    userName: faker.person.firstName(),
+    content: faker.lorem.paragraph(),
+    created_at: faker.date.past().toISOString(),
+    score: faker.helpers.rangeToNumber({ min: 1, max: 5 }),
+  })
+);
 
 export const reviewsById = http.get(
   "http://localhost:3000/reviews/:bookId",
   () => {
-    const mockReviewData: BookReviewItem[] = [
-      {
-        id: 1,
-        userName: "Nulzi",
-        content: "thank u",
-        created_at: "2024-01-02",
-        score: 5,
-      },
-      {
-        id: 2,
-        userName: "Nolza",
-        content: "sry",
-        created_at: "2024-01-12",
-        score: 2,
-      },
-    ];
     return HttpResponse.json(mockReviewData, {
       status: 200,
     });
